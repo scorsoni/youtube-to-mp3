78d56aa6a0dc07831bbb55a7a33c5690c0f4194e
William Scorsoni <willscorsoni@hotmail.com>
2026-01-22 14:31:25 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -404,4 +404,6 @@ if __name__ == '__main__':
     print(f"\n  Acesse: http://localhost:5000")
     print("=" * 60 + "\n")
 
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
+
